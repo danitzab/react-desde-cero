f34e5fd6f6c3c883d25cbbac4097b509218a6885
unknown <danitzabonilla@gmail.com>
2020-05-05 12:07:50 -0500

    Cambio de mensaje en h1 del home

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => (
           />
           <div className="main-banner__data s-center">
             <p className="t2 s-mb-0">Cursos de EDteam</p>
-            <p>Tu futuro te está esperando</p>
+            <p>Tu futuro te está esperando Marica!</p>
             <a href="https://ed.team" className="button">
               Suscribirse
             </a>
@@ -24,7 +24,6 @@ const App = () => (
     </div>
 
     <div className="ed-grid m-grid-3">
-      <Curso />
       <Curso />
       <Curso />
       <Curso />
