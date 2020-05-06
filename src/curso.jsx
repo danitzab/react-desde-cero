83679b1f945c102abe4f7a21dfe63df317d2388f
unknown <danitzabonilla@gmail.com>
2020-05-06 10:45:34 -0500

    cambios de la clase anterior

--- src/curso.jsx
+++ src/curso.jsx
@@ -1,30 +1,41 @@
 import React from "react";
+import Proptypes from "prop-types"
 
-const mayorDeEdad = (edad) => edad > 18;
-
-const persona = { nombre: "Danitza", apellido: "Bonilla", edad: "9" };
-
-const Curso = () => (
+const Curso = ({title, image, price, profesor}) => (  //componente de funcion, componente tonto, solo se limita a mostrar en la interfase pero no es logico
   <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
       <img
-        src="https://edteam-media.s3.amazonaws.com/courses/original/565fa9ec-8f84-448a-b0d8-855d059f16dd.png"
-        alt="Poster del curso"
+        src={image}
+        alt={title}
       />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      {mayorDeEdad(persona.edad) ? (
-        <h3 className="center">Hola, soy mayor</h3>
-      ) : (
-        <p>"Soy menor"</p>
-      )}
+      <h3 className="center">{title}</h3>
+      <div className="s-main-center">
+        {profesor}
+      </div>
       <div className="s-main-center">
         <a className="button--ghost-alert button--tiny" href="#">
-          $ 20USD
+          { `$ ${price} USD` }
         </a>
       </div>
     </div>
   </article>
 );
+
+Curso.prototypes = {
+  title: Proptypes.string,
+  image: Proptypes.string,
+  price: Proptypes.number,
+  profesor: Proptypes.string
+}
+
+Curso.defaultProps = {
+ title: "No se encontró titulo",
+ image: "https://www.elperiodista.cl/wp-content/uploads/2020/04/coronavirus-2.jpg",
+ price: "--",
+ profesor: ""
+
+}
 
 export default Curso;
